0fac8881ed81f49e4271e7408ce72dfa842c2b29
Diurno <clasec2comercio@gmail.com>
2025-11-19 10:04:00 +0100

    Primer Commit

---
+++ src/main/java/comunicacion/AccionJugador.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package comunicacion;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Diurno
+ */
+public class AccionJugador implements Serializable{
+    
+    public enum TipoAccion{
+        
+        MOVER_DERECHA,
+        MOVER_IZQUIERDA,
+        NADA        
+    }
+    
+    
+    private TipoAccion tipoAccion;
+    
+    private int idJugador;
+
+    public AccionJugador(TipoAccion tipoAccion, int idJugador) {
+        this.tipoAccion = tipoAccion;
+        this.idJugador = idJugador;
+    }
+
+    public TipoAccion getTipoAccion() {
+        return tipoAccion;
+    }
+
+    public void setTipoAccion(TipoAccion tipoAccion) {
+        this.tipoAccion = tipoAccion;
+    }
+
+    public int getIdJugador() {
+        return idJugador;
+    }
+
+    public void setIdJugador(int idJugador) {
+        this.idJugador = idJugador;
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ src/main/java/comunicacion/EstadoJuego.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package comunicacion;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Diurno
+ */
+public class EstadoJuego implements Serializable{
+    
+    private double pelotaX;
+    private double pelotaY;
+    private double pelotaTamaño;
+    
+    
+    private Ño
+}
